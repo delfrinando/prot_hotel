51e638043b4e19d327c4105e5f4a7054e058ad8f
Delfrinando <delfrinando@gmail.com>
2017-01-12 15:37:20 +0700

    #

--- local-hotel-publisher.js
+++ local-hotel-publisher.js
@@ -20,6 +20,7 @@ app.get('/', function(req, res) {
           const datime = new Date().getTime() - startp;
           console.log('[.] Got message! in ' + datime);
           res.end(msg.content.toString());
+          
           ch.ack(msg, function(){            
             ch.close(function(err, conn){
               conn.close();
